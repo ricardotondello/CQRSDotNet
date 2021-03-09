2e9c354700e7731a566c3c67fca9a025ba0e74dd
Ricardo Tondello <rkdtondello@gmail.com>
2021-03-09 19:36:30 +0100

    fix: Domain visibility

--- Cqrs/Database/Repository.cs
+++ Cqrs/Database/Repository.cs
@@ -7,10 +7,11 @@ namespace Cqrs.Database
     public class Repository
     {
         public List<Domain.Person> People { get; } =
-            new Faker<Domain.Person>()
-                .RuleFor(x => x.Id, Guid.NewGuid())
-                .RuleFor(x => x.Name, f => f.Person.FirstName)
-                .RuleFor(x => x.Surname, f => f.Person.LastName)
-                .RuleFor(x => x.DateOfBirth, f => f.Person.DateOfBirth).Generate(100);
+            new Faker<Domain.Person>().
+            CustomInstantiator(f => new Domain.Person(Guid.NewGuid(), 
+                                                        f.Person.FirstName, 
+                                                        f.Person.LastName, 
+                                                        f.Person.DateOfBirth))
+                .Generate(100);
     }
 }
--- Cqrs/Domain/Person.cs
+++ Cqrs/Domain/Person.cs
@@ -7,9 +7,22 @@ namespace Cqrs.Domain
 {
     public class Person
     {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public Guid Id { get; }
+        public string Name { get; }
+        public string Surname { get; }
+        public DateTime DateOfBirth { get; }
+
+        private Person(){ }
+        public Person(Guid id, string name, string surname, DateTime dateOfBirth)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            DateOfBirth = dateOfBirth;
+        }
     }
+
+    
+    
+
 }
